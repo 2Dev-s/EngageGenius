ac85f70181759e25fbdd53f7e931b179be7f6840
Dani Dumitrache <76882185+DaniDumitrache@users.noreply.github.com>
2024-04-01 16:16:08 +0300

    fixed minior problems && added facebook api

--- app/Http/Controllers/TwitterAPIController.php
+++ app/Http/Controllers/TwitterAPIController.php
@@ -9,32 +9,52 @@ use Illuminate\Support\Facades\Auth;
 class TwitterAPIController extends Controller
 {
     //
-    public function getOauth(Request $request) {
+    public function getOauth(Request $request)
+    {
 
         session('oauth_token', '');
-        session('koauth_token_secretey', '');
+        session('oauth_token_secret', '');
 
         $connection = new TwitterOAuth("kwA8XtEyiPGOdkvN3gJtNgToA", "YXEnfShva7emo93e98VlqUnkvUqaoCfJ0ennqYLEkAYcOSCTPU");
-        $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => 'http://192.168.0.176/twitter/callback'));
+        $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => route('twitter-callback')));
 
         session(['oauth_token' => $request_token['oauth_token']]);
         session(['oauth_token_secret' => $request_token['oauth_token_secret']]);
 
         return redirect($connection->url('oauth/authorize', ['oauth_token' => $request_token['oauth_token']]));
     }
-    public  function callback(Request $request){
+
+    public  function callback(Request $request)
+    {
         $request_token['oauth_token'] = session('oauth_token');
         $request_token['oauth_token_secret'] = session('oauth_token_secret');
 
-        $connection = new TwitterOAuth("kwA8XtEyiPGOdkvN3gJtNgToA","YXEnfShva7emo93e98VlqUnkvUqaoCfJ0ennqYLEkAYcOSCTPU", $request_token['oauth_token'], $request_token['oauth_token_secret']); // app as user
+        $connection = new TwitterOAuth("kwA8XtEyiPGOdkvN3gJtNgToA", "YXEnfShva7emo93e98VlqUnkvUqaoCfJ0ennqYLEkAYcOSCTPU", $request_token['oauth_token'], $request_token['oauth_token_secret']); // app as user
         $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $request->all()['oauth_verifier']]);
 
         Auth::user()->currentTeam->socialData()->update([
             'twitter_access_token' => $access_token['oauth_token'],
             'twitter_access_token_secret' => $access_token['oauth_token_secret'],
         ]);
+    }
 
+    public function post()
+    {
+        $request_token['oauth_token'] = session('oauth_token');
+        $request_token['oauth_token_secret'] = session('oauth_token_secret');
 
-        return;
+        $connection = new TwitterOAuth("kwA8XtEyiPGOdkvN3gJtNgToA", "YXEnfShva7emo93e98VlqUnkvUqaoCfJ0ennqYLEkAYcOSCTPU", $request_token['oauth_token'], $request_token['oauth_token_secret']); // app as user
+        $connection->setApiVersion(2);
+
+        /*
+        ERROR: [
+            'title' => 'unauthorized',
+            'type' => 'unauthorized'
+            ]
+        */
+
+        $result = $connection->post('tweets', ['text' => 'Hello World!'], ['jsonPayload' => true]);
+
+        return $result;
     }
 }
--- routes/externalapis.php
+++ routes/externalapis.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TwitterAPIController;
 use App\Http\Controllers\LinkedinApiController;
+use App\Http\Controllers\FacebookApiController;
 use App\Http\Controllers\UnsplashApiController;
 
 Route::prefix('linkedin')->group(function () {
@@ -15,4 +16,9 @@ Route::prefix("twitter")->group(function () {
     Route::get('/oauth', [TwitterAPIController::class, 'getOauth'])->middleware(['auth'])->name('twitter-oauth');
     Route::get('/callback', [TwitterAPIController::class, 'callback'])->name('twitter-callback');
     Route::get('/post', [TwitterAPIController::class, 'post']);
+});
+
+Route::prefix('facebook')->group(function () {
+    Route::get('/oauth', [FacebookApiController::class, 'getOauth'])->middleware(['auth'])->name('facebook-oauth');
+    Route::get('/callback', [FacebookApiController::class, 'callback'])->name('facebook-callback');
 });
